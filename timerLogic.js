f14504dd883e40ce8c6d2d8a35a864a9547dacf5
Humayra98 <humayraanjum98@gmail.com>
2021-12-17 01:50:30 -0500

    break time logic done

--- timerLogic.js
+++ timerLogic.js
@@ -2,6 +2,7 @@
 const input_hr = 1;
 const input_min = 20;
 const input_sec = 50;
+const BREAK_PERCENT = 0.25;
 
 const convert_to_secs = (input_hr, input_min, input_sec) => {
     return Number(input_hr)*60*60 + Number(input_min)*60 + Number(input_sec);
@@ -26,6 +27,9 @@ console.log(`test2: [hr:min:sec] ${test2[0]} : ${test2[1]} : ${test2[2]}`);
 const hr_elem = document.querySelector('#hr');
 const min_elem = document.querySelector('#min');
 const sec_elem= document.querySelector('#sec');
+const br_hr = document.querySelector('#br-hr');
+const br_min = document.querySelector('#br-min');
+const br_sec= document.querySelector('#br-sec');
 const start_pause_btn = document.querySelector('#start-pause');
 const stop_btn = document.querySelector('#stop');
 const time_input_elem = document.querySelector('#input-time');
@@ -54,8 +58,11 @@ time_inputs.forEach(elem => {
 
 
 var time = 0; //in seconds, time left
+var br_time = 0;
 
 let intervalID = null; //countdown interval
+let br_intervalID = null; //countdown interval
+
 
 // let intervalID = null; //countdown interval
 
@@ -66,7 +73,7 @@ const STOPPED = 2;
 const EDIT = 3;
 const READY = 4;
 
-//colors
+//button colors
 const disabled_background = 'rgb(238, 225, 218)';
 const disabled_text = "rgb(158, 150, 147)";
 const enabled_text = "rgb(54, 38, 30)";
@@ -105,6 +112,9 @@ start_pause_btn.addEventListener('click', () => {
         state = RUNNING;
         countdown();//start countdown immediately when clicked
         intervalID = setInterval(countdown, 1000);
+
+        clearInterval(br_intervalID);//pause br timer
+
     } 
     
     else { //if running, paused
@@ -120,6 +130,10 @@ start_pause_btn.addEventListener('click', () => {
         }, 100);
 
         start_pause_btn.textContent = "Resume";
+
+        br_countdown();//start br countdown immediately when clicked
+        br_intervalID = setInterval(br_countdown, 1000);
+
 
     }
 
@@ -163,11 +177,19 @@ stop_btn.addEventListener('click', () => {
         time_input_elem.style.display = "none";
         //input field disappears, and main field appears with set time
         time = total_secs;
+        br_time = BREAK_PERCENT*time;
 
+        // set main time
         const [hr, min, sec] = convert_from_secs(time);
         hr_elem.textContent = String(hr).padStart(2,'0');
         min_elem.textContent = String(min).padStart(2,'0');
         sec_elem.textContent = String(sec).padStart(2,'0');
+
+        //set break time
+        const [br_hr_val, br_min_val, br_sec_val] = convert_from_secs(br_time);
+        br_hr.textContent = String(br_hr_val).padStart(2,'0');
+        br_min.textContent = String(br_min_val).padStart(2,'0');
+        br_sec.textContent = String(br_sec_val).padStart(2,'0');
 
         start_pause_btn.disabled = false;
         start_pause_btn.style.backgroundColor = main_btn_color;
@@ -209,11 +231,18 @@ stop_btn.addEventListener('click', () => {
         min_elem.textContent = '00';
         sec_elem.textContent = '00';
 
+        //reset break timer
+        br_hr.textContent = '00';
+        br_min.textContent = '00';
+        br_sec.textContent = '00';
+
         clearInterval(intervalID);
+        clearInterval(br_intervalID);
         setProgress(0);
         state = STOPPED;
 
         time = 0;
+        br_time = 0;
     }
 
 });
@@ -240,6 +269,7 @@ const countdown = () => {
 
         //stop decrementing timer
         clearInterval(intervalID);
+        clearInterval(br_intervalID);
         setProgress(0);
     }
     else {
@@ -270,6 +300,7 @@ const countdown = () => {
         sec_elem.textContent = '00';
 
         clearInterval(intervalID);
+        clearInterval(br_intervalID);
         setProgress(100);
         state = STOPPED;
         setTimeout(() => {
@@ -277,6 +308,47 @@ const countdown = () => {
         }, 1000);
 
         time = 0;
+    }
+};
+
+const br_countdown = () => {
+    console.log("break time:" , br_time);
+
+    const [br_hr_val, br_min_val, br_sec_val] = convert_from_secs(br_time);
+
+    if (br_time === 0) {
+        console.log("Break Time over! Focus time running.");
+    }
+
+    if (br_time === 60) {
+        console.log("1 minute break remaining. Focus time will resume automatically after this.");
+    }
+    if(br_time <= 0) {
+        br_hr.textContent = '00';
+        br_min.textContent = '00';
+        br_sec.textContent = '00';
+
+        //stop decrementing timer
+        clearInterval(br_intervalID);
+    }
+    else {
+        br_hr.textContent = String(br_hr_val).padStart(2,'0');
+        br_min.textContent = String(br_min_val).padStart(2,'0');
+        br_sec.textContent = String(br_sec_val).padStart(2,'0');
+    }
+
+    br_time--;
+    if (br_time == -1) {//change to STOPPED state once time is up
+        
+        console.log('AFTER stop break timer');
+
+        //stop decrementing timer
+        br_hr.textContent = '00';
+        br_min.textContent = '00';
+        br_sec.textContent = '00';
+
+        clearInterval(br_intervalID);
+        br_time = 0;
     }
 };
 
